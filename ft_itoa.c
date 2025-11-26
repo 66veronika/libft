156d9e04c1429764a446b3872d80dbf2707cca59
Veronika <vee66veen@gmail.com>
2025-11-26 23:38:24 +0100

    .

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,72 @@
 /*   By: veronikaskopova <veronikaskopova@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 01:13:10 by veronikasko       #+#    #+#             */
-/*   Updated: 2025/11/25 01:13:11 by veronikasko      ###   ########.fr       */
+/*   Updated: 2025/11/26 17:28:22 by veronikasko      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+static int	n_len(long n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+static void	convert(long num, char *res, int len)
+{
+	if (num == 0)
+	{
+		res[0] = '0';
+		return ;
+	}
+	if (num < 0)
+	{
+		res[0] = '-';
+		num = -num;
+	}
+	while (num > 0)
+	{
+		res[--len] = (num % 10) + '0';
+		num /= 10;
+	}
+}
+char	*ft_itoa(int n)
+{
+	char	*res;
+	long	num;
+	int		len;
+
+	num = n;
+	len = n_len(num);
+	res = malloc(len + 1);
+	if (!res)
+		return (NULL);
+	res[len] = '\0'; 
+	convert(num, res, len);
+	return (res);
+}
+
+/*
+int main(void)
+{
+    int nums[] = {0, 5, -5, 42, -42, 12345, -2147483648};
+    int i = 0;
+
+    while (i < 7)
+    {
+        char *s = ft_itoa(nums[i]);
+        printf("ft_itoa(%d) = %s\n", nums[i], s);
+        free(s);
+        i++;
+    }
+    return 0;
+}*/
---
+++ ft_split.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: veronikaskopova <veronikaskopova@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 22:07:26 by veronikasko       #+#    #+#             */
+/*   Updated: 2025/11/26 23:37:50 by veronikasko      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+static int	word_count(char const *s, char c)
+{
+	int	i;
+	int	count;
+	int	in_word;
+
+	i = 0;
+	count = 0;
+	in_word = 0;
+	while (s[i])
+	{
+		if (s[i] != c && in_word == 0)
+		{
+			count++;
+			in_word = 1;
+		}
+		else if (s[i] == c)
+			in_word = 0;
+		i++;
+	}
+	return (count);
+}
+
+static int	word_len(char const *s, char c)
+{
+	int	len;
+
+	len = 0;
+	while (s[len] && s[len] != c)
+	{
+		len++;
+	}
+	return (len);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+	int		i;
+	int		j;
+	int		k;
+	int		w_len;
+	
+	
+	split = malloc((word_count(s, c) + 1)* sizeof(char *));
+	i = 0;
+	j = 0;
+	if (!s)
+		return (NULL);
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		w_len = word_len(s + i, c);
+		split[j] = malloc(w_len + 1);
+		if (!split[j])
+			return (NULL);
+		k = 0;
+		while (k < w_len)
+		{
+			split[j][k] = s[i + k];
+			k++;
+		}
+		split[j][w_len] = '\0';
+		i += w_len;
+		j++;
+	}
+	split[j] = NULL;
+	return (split);
+}
+/*
+int	main(void)
+{
+	char	**split = ft_split("Hello world hi!", ' ');
+	int		i = 0;
+	while (split[i])
+	{
+		printf("%s\n", split[i]);
+		i++;
+	}
+	i = 0;
+    while (split[i])
+    {
+        free(split[i]);
+        i++;
+    }
+    free(split);
+	return (0);
+}*/
